9702681874f3c7ad639d66c7f3e483309919d763
raphael <raphaelbunck@gmail.com>
2017-07-17 22:59:40 +0200

    App.java resycled code

--- src/main/java/nl/rabaelo/soundsharp/App.java
+++ src/main/java/nl/rabaelo/soundsharp/App.java
@@ -1,13 +1,45 @@
 package nl.rabaelo.soundsharp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.Console;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        String name = welcome();
+        System.out.println("What is your password?");
+
+        if (logIn()) {
+            System.out.println("Welcome to SoundSharp, " + name);
+        }
+    }
+
+    private static String welcome() {
+        Scanner scan = new Scanner(System.in);
+        return (scan.next());
+    }
+
+
+    private static boolean logIn() {
+        Console console = System.console();
+        int x = 0;
+        System.out.println("Try: " + "(" + (3-x) + "/3)");
+        while (x < 3) {
+            char[] password = console.readPassword();
+            char[] correctPass = new char[]{'S', 'H', 'A', 'R', 'P', 'S', 'O', 'U', 'N', 'D'};
+            if (Arrays.equals(password, correctPass)) {
+                return true;
+            } else {
+                System.out.println("Password incorrect.");
+                x++;
+            }
+            System.out.println("Try: " + "(" + (3-x) + "/3)");
+
+            if (x==2){
+                System.out.println("Last try!");
+            }
+        }
+        return false;
     }
 }
